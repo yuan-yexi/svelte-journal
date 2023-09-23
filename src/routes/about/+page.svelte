52b94586d40249c79e05592659180d03b6435cdd
yexi-yuan <yuanyexi1@gmail.com>
2023-09-24 01:24:31 +0800

    added dynamic routing | server.js load data

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,2 @@
+<h1>About Svelte Kit</h1>
+<p>This is the about page.</p>
---
+++ src/routes/journal/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let data;
+</script>
+
+<h1>Journal</h1>
+
+<ul>
+  {#each data.summaries as {slug, title} }
+    <li><a href="/journal/{slug}">{title}</a></li>
+  {/each}
+</ul>
