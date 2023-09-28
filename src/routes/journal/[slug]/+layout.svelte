649672e1c8953355d3c9490ae66b3cb871645c08
yexi-yuan <yuanyexi1@gmail.com>
2023-09-28 21:42:23 +0800

    Added svelte layout for joural pages

---
+++ src/routes/journal/[slug]/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	export let data;
+</script>
+
+<div>
+	<main>
+		<slot />
+	</main>
+</div>
+<aside>
+	<h2>More Posts</h2>
+	<ul>
+		{#each data.summaries as { slug, title }}
+			<li>
+				<a href="/journal/{slug}">{title}</a>
+			</li>
+		{/each}
+	</ul>
+</aside>
--- src/routes/journal/[slug]/+page.svelte
+++ src/routes/journal/[slug]/+page.svelte
@@ -3,5 +3,5 @@
 </script>
 
 <h1>Journal Post</h1>
-<h1>{data.post.title}</h1>
+  <h1>{data.post.title}</h1>
 <div>{@html data.post.content}</div>
